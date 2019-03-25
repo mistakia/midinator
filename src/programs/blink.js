f47a7a26c8f4f57c55c320c3fda477b7adc422e5
Kia Rahimian <kia@tintmail.com>
2019-03-25 13:01:30 -0400

    feat: save/load project + program params

--- src/programs/blink.js
+++ src/programs/blink.js
@@ -1,6 +1,39 @@
-module.exports = function({ canvas, ctx, delta }) {
-  console.log('program')
-  ctx.fillStyle = `rgba(255,255,255,${1-delta})`
+const run = ({ canvas, ctx, delta, color, length }) => {
+  //TODO: validate params
+  color = color || '255,255,255'
+  length = length || 10
+
+  const opacity = 1 - (delta / length)
+  if (opacity <= 0) return
+  ctx.fillStyle = `rgba(${color},${opacity})`
   ctx.fillRect(0,0, 600, 100)
-  //fs.writeFileSync('frames/' + leftpad(start, 5) + '.png', canvas.toBuffer())
+}
+
+const renderParams = ({ params, document }) => {
+  const container = document.createElement('div')
+  const colorLabel = document.createElement('label')
+  colorLabel.innerHTML = 'Color:'
+  const colorInput = document.createElement('input')
+  colorInput.value = params.color || '255,255,255'
+  colorInput.oninput = () => {
+    params.color = colorInput.value
+  }
+  container.appendChild(colorLabel)
+  container.appendChild(colorInput)
+
+  const lengthLabel = document.createElement('label')
+  lengthLabel.innerHTML = 'Length:'
+  const lengthInput = document.createElement('input')
+  lengthInput.value = params.length || 10
+  lengthInput.oninput = () => {
+    params.length = lengthInput.value
+  }
+  container.appendChild(lengthLabel)
+  container.appendChild(lengthInput)
+  return container
+}
+
+module.exports = {
+  renderParams,
+  run
 }
