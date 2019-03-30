8a293f84108ff91b5ac4745d016fe9d85002e98e
mistakia <1823355+mistakia@users.noreply.github.com>
2019-03-30 12:17:19 -0400

    feat: import .mov as program

--- src/programs/index.js
+++ src/programs/index.js
@@ -1,5 +1,40 @@
-module.exports = {
-  'fade': require('./fade'),
-  'fill': require('./fill'),
-  'slide': require('./slide')
+const ffmpeg = require('fluent-ffmpeg')
+
+const Project = require('../project')
+const Program = require('../program')
+const progressElem = document.getElementById('progress')
+
+let Programs = {
+  _index: {
+    'fade': require('./fade'),
+    'fill': require('./fill'),
+    'slide': require('./slide')
+  }
 }
+
+Programs.run = function (name, params) {
+  return this._index[name].run(params)
+}
+
+Programs.renderParams = function(name, params) {
+  return this._index[name].renderParams(params)
+}
+
+Programs.load = async function(filepaths) {
+  progressElem.removeAttribute('value')
+  for (let i=0; i < filepaths.length; i++) {
+    const filepath = filepaths[i]
+    const program = new Program(filepath)
+    if (!this._index[program.filename]) {
+      await program.load()
+      this._index[program.filename] = program
+    }
+  }
+  progressElem.value = 0
+}
+
+Programs.list = function () {
+  return Object.keys(this._index)
+}
+
+module.exports = Programs
--- src/project.js
+++ src/project.js
@@ -4,11 +4,13 @@ const projectFile = localStorage.getItem('projectFile')
 
 let Project = {
   midiFile: '',
-  midiEvents: []
+  midiEvents: [],
+  programs: []
 }
 
 if (projectFile) {
   Project = jsonfile.readFileSync(projectFile)
+  if (!Project.programs) Project.programs = [] //backwards compatability
 }
 
 module.exports = Project
