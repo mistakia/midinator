1040de4176a8d1b0fe401979dd677044b2c33f22
mistakia <1823355+mistakia@users.noreply.github.com>
2019-04-06 19:28:40 -0400

    feat: add manual input to param class

--- src/utils.js
+++ src/utils.js
@@ -13,7 +13,18 @@ const clearProgramParams = () => {
   programParams.innerHTML = ''
 }
 
-const renderInput = ({ label, input, parent, min, max, value, step, oninput }) => {
+const renderInput = ({
+  label,
+  input,
+  parent,
+  range,
+  manual,
+  min,
+  max,
+  value,
+  step,
+  oninput
+}) => {
   const programParamContainer = document.createElement('div')
   programParamContainer.className = 'param-input'
 
@@ -21,17 +32,33 @@ const renderInput = ({ label, input, parent, min, max, value, step, oninput }) =
   labelElem.innerHTML = label
   programParamContainer.appendChild(labelElem)
 
-  if (!input) {
-    const inputElem = document.createElement('input')
-    inputElem.type = 'range'
-    if (isDefined(max)) inputElem.max = max
-    if (isDefined(min)) inputElem.min = min
-    if (isDefined(step)) inputElem.step = step
-    if (isDefined(value)) inputElem.value = value
-    inputElem.oninput = () => oninput(inputElem.value)
-    programParamContainer.appendChild(inputElem)
-  } else {
+  let inputElem
+  let rangeElem
+
+  if (input) {
     programParamContainer.appendChild(input)
+  } else if (manual) {
+    inputElem = document.createElement('input')
+    inputElem.value = value
+    inputElem.oninput = () => {
+      oninput(inputElem.value)
+      if (rangeElem) rangeElem.value = inputElem.value
+    }
+    programParamContainer.appendChild(inputElem)
+  }
+
+  if (range) {
+    rangeElem = document.createElement('input')
+    rangeElem.type = 'range'
+    if (isDefined(max)) rangeElem.max = max
+    if (isDefined(min)) rangeElem.min = min
+    if (isDefined(step)) rangeElem.step = step
+    if (isDefined(value)) rangeElem.value = value
+    rangeElem.oninput = () => {
+      oninput(rangeElem.value)
+      if (inputElem) inputElem.value = rangeElem.value
+    }
+    programParamContainer.appendChild(rangeElem)
   }
 
   parent.appendChild(programParamContainer)
@@ -47,6 +74,8 @@ const renderParam = ({ name, param, parent, min, max, step }) => {
   renderInput({
     label: 'Start:', min, max, step,
     value: p.start,
+    manual: true,
+    range: true,
     oninput: (value) => {
       param.start = parseInt(value, 10)
     },
@@ -56,6 +85,8 @@ const renderParam = ({ name, param, parent, min, max, step }) => {
   renderInput({
     label: 'End:', min, max, step,
     value: p.end,
+    manual: true,
+    range: true,
     oninput: (value) => {
       param.end = parseInt(value, 10)
     },
